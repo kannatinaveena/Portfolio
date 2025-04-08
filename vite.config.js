be628f5865c0dcafb9ba1ebead7cd42bfb863fdb
kannatinaveena <naveena.kannati@sasi.ac.in>
2025-04-08 22:24:06 +0530

    Commits

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfoliokn/', // must match repo name exactly
+  base: '/Portfolio/', // must match repo name exactly
   plugins: [react()],
 });
